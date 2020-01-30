0eaf2655c458de47fbea04acad34d20e2a6b1709
Sami Ullah <samiullah.khan.1989@gmail.com>
2020-01-30 17:38:28 +0400

    fixed add form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Observable } from 'rxjs';
+import { debug } from 'util';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,22 @@ export class HomePage {
 
   items: Observable<any[]>;
 
+  newTodo: string = '';
+
   constructor(private db: AngularFirestore ) {
     this.items = db.collection('items').valueChanges();
+  }
+
+  addTodo(){
+    debug
+    this.db.collection('items').add({
+      title: this.newTodo
+    }).then(resp => {
+      console.log(resp);
+      
+    }).catch(error => {
+      console.log(error);
+      
+    })
   }
 }
